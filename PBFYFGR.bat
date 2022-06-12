1de67fa9dce8fb1b8334621b37c86d8946c2dda6
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-06-12 22:32:58 +0200

    Update PBFYFGR.bat

--- PBFYFGR.bat
+++ PBFYFGR.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://google.com https://heeeeeeeey.com/ https://f5.dk --chrome --fullscreen --kiosk
+:: public
+"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/VjYM8aW1ScIs --chrome --fullscreen --kiosk
