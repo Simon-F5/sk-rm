599e417fe7cd24165d169c819034cd7868e59480
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-06-19 13:56:46 +0200

    Update PB02A40Y.bat

--- PB02A40Y.bat
+++ PB02A40Y.bat
@@ -1,7 +1,7 @@
 :: Talent
 "C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/Q0YJGJIhJ8x5 --chrome --fullscreen --kiosk
 timeout /t 10800
-tskill "C:\Program Files\Google\Chrome\Application\chrome.exe"
+tskill chrome
 timeout /t 2
 for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
 %serialnumber%.bat
