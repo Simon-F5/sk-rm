ad2a4fb950c6ca0ea127835498ec306c251c827a
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-06-19 13:57:11 +0200

    added restart every 3 hours

--- PB02A42M.bat
+++ PB02A42M.bat
@@ -1,2 +1,7 @@
 :: Medlemsservice
 "C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM --chrome --fullscreen --kiosk
+timeout /t 10800
+tskill chrome
+timeout /t 2
+for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
+%serialnumber%.bat
