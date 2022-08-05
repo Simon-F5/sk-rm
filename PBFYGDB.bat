45784719cf51d41cfad8b48cc5901a4ee0cd7076
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-08-05 10:14:31 +0200

    Updatet link

--- PBFYGDB.bat
+++ PBFYGDB.bat
@@ -1,5 +1,5 @@
 :: Private
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/OBcg41M9S3Wz --chrome --fullscreen --kiosk
+"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/qdR2IM6ULaET --chrome --fullscreen --kiosk
 timeout /t 10800
 tskill chrome
 timeout /t 2
