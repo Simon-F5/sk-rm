067dfe445a63a13a705aa2cdce98410e07ac9f6b
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-08-24 13:08:20 +0200

    Updatet url

--- PBFYFGR.bat
+++ PBFYFGR.bat
@@ -1,3 +1,7 @@
-:: public
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/VjYM8aW1ScIs --chrome --fullscreen --kiosk
-
+:: public 2
+"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/bKXjpeWELSCn --chrome --fullscreen --kiosk
+        timeout /t 10800
+        tskill chrome
+        timeout /t 2
+        for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
+        %serialnumber%.bat
