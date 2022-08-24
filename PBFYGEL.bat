c078437ca6c4a55d79e3f9a22f17907ac740adaf
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-08-24 13:07:46 +0200

    Updatet url

--- PBFYGEL.bat
+++ PBFYGEL.bat
@@ -1,7 +1,7 @@
-:: public
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/VjYM8aW1ScIs --chrome --fullscreen --kiosk
-timeout /t 10800
-tskill chrome
-timeout /t 2
-for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
-%serialnumber%.bat
+:: public 1
+"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/Q0YJGJIhJ8x5 --chrome --fullscreen --kiosk
+        timeout /t 10800
+        tskill chrome
+        timeout /t 2
+        for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
+        %serialnumber%.bat
