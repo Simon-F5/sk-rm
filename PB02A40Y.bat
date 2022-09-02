55b42119060fb0730855ef3484bdd6b2e2016842
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-09-02 11:23:59 +0200

    Updatet URL

--- PB02A40Y.bat
+++ PB02A40Y.bat
@@ -1,5 +1,5 @@
-:: Talent
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/ZPd3MZiwSzUj https://f5aps.clicdata.com/v/FinHeASKuzhM https://f5aps.clicdata.com/v/Q0YJGJIhJ8x5 --chrome --fullscreen --kiosk
+:: Skærm 2
+"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/Q0YJGJIhJ8x5 --chrome --fullscreen --kiosk
 timeout /t 10800
 tskill chrome
 timeout /t 2
