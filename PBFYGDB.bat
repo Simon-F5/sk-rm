f1540f53a702447b39776beea2c1740429b9a66a
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-09-08 08:09:42 +0200

    Delete PBFYGDB.bat

--- PBFYGDB.bat
+++
@@ -1,7 +0,0 @@
-:: Private
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/qdR2IM6ULaET --chrome --fullscreen --kiosk
-timeout /t 10800
-tskill chrome
-timeout /t 2
-for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
-%serialnumber%.bat
