a8f80a3c56329a6e6cf93de181572a2b47ecc501
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-09-08 08:12:00 +0200

    Delete PB6VXMZ.bat

--- PB6VXMZ.bat
+++
@@ -1,7 +0,0 @@
-:: Skærm 1
-"C:\Program Files\Google\Chrome\Application\chrome.exe" https://f5aps.clicdata.com/v/FinHeASKuzhM --chrome --fullscreen --kiosk
-timeout /t 10800
-tskill chrome
-timeout /t 2
-for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
-%serialnumber%.bat
