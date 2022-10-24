ef85bce3daf3de2e85a672447d4f541c4352f309
Simon-F5 <107346251+Simon-F5@users.noreply.github.com>
2022-10-24 18:53:20 +0200

    Delete Start.bat

--- Start.bat
+++
@@ -1,7 +0,0 @@
-timeout /t 30
-cd \Users\simon.s\
-rmdir /s /q C:\Users\simon.s\sk-rm
-git clone https://github.com/Simon-F5/sk-rm.git
-for /f "delims=" %%i in ('wmic bios get serialnumber /value') do for /f "delims=" %%j in ("%%i") do set "%%j"
-cd sk-rm 
-%serialnumber%.bat
